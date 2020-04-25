19d4bf8d0d4bc32742a72643a7cba28b0dd69089
Arthur Golubtsov <goldartt@gmail.com>
2020-04-26 02:30:11 +0300

    Insert calculating px4 home position

--- scripts/export_env.sh
+++ scripts/export_env.sh
@@ -3,3 +3,8 @@
 while IFS= read -rd '' var; do
     export "$var"
 done </proc/1/environ
+
+`python /scripts/calculate_gps.py`
+
+echo "PX4_HOME_LAT=$PX4_HOME_LAT"
+echo "PX4_HOME_LON=$PX4_HOME_LON"
